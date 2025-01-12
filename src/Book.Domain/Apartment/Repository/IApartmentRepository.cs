2bcc356fc5d9579d5b577d41abebc79bca48e968
hp <hp@AIDA>
2025-01-12 15:34:41 +0330

    Add Repositories interfaces and UnitOfWork Interface

---
+++ src/Book.Domain/Apartment/Repository/IApartmentRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Book.Domain.Apartment.Repository
+{
+    public interface IApartmentRepository
+    {
+        Task<Apartment> GetByIdAsync(Guid id , CancellationToken cancellationToken = default);
+    }
+}
