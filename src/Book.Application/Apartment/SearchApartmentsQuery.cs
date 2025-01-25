5abe24423b8f2f2a7555b9b6b6950bb9d7a28806
hp <hp@AIDA>
2025-01-25 17:34:35 +0330

    SearchApartment Query implemented

---
+++ src/Book.Application/Apartment/SearchApartmentsQuery.cs
@@ -0,0 +1,7 @@
+﻿using Book.Application.Abstraction.Messaging;
+
+namespace Book.Application.Apartment
+{
+    internal sealed record SearchApartmentsQuery(DateOnly startDate, DateOnly endDate)
+        : IQuery<IReadOnlyList<ApartmentResponse>>{ };
+}
