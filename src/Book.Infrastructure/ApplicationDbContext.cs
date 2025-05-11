5dc27a516b75f5462c9de28413ebe7a86e92bf32
Aida <ayda.alipoor81.aa@gmail.com>
2025-05-12 00:49:05 +0330

    Add EventPublisher to DbContext

--- src/Book.Infrastructure/ApplicationDbContext.cs
+++ src/Book.Infrastructure/ApplicationDbContext.cs
@@ -1,17 +1,47 @@
 ﻿using Book.Domain.Abstraction;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Book.Infrastructure
 {
-    public sealed class ApplicationDbContext : DbContext , IUnitOfWork
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContext) 
+        private readonly IPublisher _publisher;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContext)
             : base(dbContext) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            await PublishDomainEventAsync();
+
+            return result;
+        }
+
+        private async Task PublishDomainEventAsync()
+        {
+            var domainEvents = ChangeTracker.Entries<Entity>()
+                .Select(entry => entry.Entity)
+                .SelectMany(entity =>
+                {
+                    var domainEvent = entity.GetDomainEvents();
+
+                    entity.ClearDomainEvents();
+
+                    return domainEvent;
+                }).ToList();
+
+            foreach (var domainEvent in domainEvents)
+            {
+                await _publisher.Publish(domainEvent);
+            }
         }
     }
 }
